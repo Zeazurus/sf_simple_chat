84b0c6d970656d361ad5cdf10e873399fc593a80
Zeazurus <Zeazurus@gmail.com>
2022-11-15 23:18:07 +0700

    Created simply chat

---
+++ src/ChatServer.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ChatServer {
+    ArrayList<Client> clients = new ArrayList<>();
+    ServerSocket serverSocket;
+
+    ChatServer() throws IOException {
+        // создаем серверный сокет на порту 1234
+        serverSocket = new ServerSocket(1234);
+    }
+
+    void sendAll(String message) {
+        for (Client client : clients) {
+            client.recieve(message);
+        }
+    }
+
+    public void run() {
+        while (true) {
+            System.out.println("Waiting...");
+            // ждем клиента из сети
+            try {
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected!");
+                clients.add(new Client(socket, this));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new ChatServer().run();
+    }
+}
